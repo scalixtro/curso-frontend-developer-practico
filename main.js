34b64eec4e84a70e43a4078da04c5eab720c8a3c
Said Calixtro <saidcalixtro@gmail.com>
2022-09-11 15:21:51 -0500

    Added product list

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+function createProductCards(productList) {
+    for(product of productList) {
+        const productCard = document.createElement('div');
+        const img = document.createElement('img');
+        const productInfo = document.createElement('div')
+        const productInfoDiv = document.createElement('div');
+        const productInfoFig = document.createElement('figure');
+    
+        productCard.classList.add('product-card');
+        img.setAttribute('src', product.image);
+        productInfo.classList.add('product-info');
+        productInfoDiv.innerHTML = `<p>\$${product.price}</p>
+                                    <p>${product.name}</p>`;
+        productInfoFig.innerHTML = '<img src="./icons/bt_add_to_cart.svg" alt="">';
+        productInfo.append(productInfoDiv, productInfoFig);
+        productCard.append(img, productInfo);
+        cardsContainer.appendChild(productCard)
+    }
+}
+
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const navShoppingCart = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 // Media query
 const hamburgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
@@ -30,3 +51,24 @@ navShoppingCart.addEventListener('click', e => {
         desktopMenu.classList.add('inactive');
     }
 })
+
+// Products
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "TV",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "PC",
+    price: 300,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+createProductCards(productList);
