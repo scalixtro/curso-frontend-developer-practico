6b21a94828ecf6ab8c71d408be678cb458dbba31
Said Calixtro <saidcalixtro@gmail.com>
2022-09-11 14:10:31 -0500

    Added mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+// Media query
+const hamburgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener('click', e => {
     desktopMenu.classList.toggle("inactive");
+})
+
+hamburgerMenu.addEventListener('click', e => {
+    mobileMenu.classList.toggle("inactive")
 })
