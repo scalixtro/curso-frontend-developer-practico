9ed7ad60e4e6d8830eee05667582b14aad00c83c
Said Calixtro <saidcalixtro@gmail.com>
2022-09-11 14:39:24 -0500

    Added shopping cart

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const navShoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 // Media query
 const hamburgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener('click', e => {
     desktopMenu.classList.toggle("inactive");
+
+    if(!desktopMenu.classList.contains('inactive')) {
+        productDetail.classList.add('inactive');
+    }
 })
 
 hamburgerMenu.addEventListener('click', e => {
     mobileMenu.classList.toggle("inactive")
+
+    if(!mobileMenu.classList.contains('inactive')){
+        productDetail.classList.add('inactive');
+    }
+})
+
+navShoppingCart.addEventListener('click', e => {
+    productDetail.classList.toggle("inactive");
+
+    if (!productDetail.classList.contains("inactive")) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
 })
